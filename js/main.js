a6026e110f8beb15c465c5e1af7682fff52d3ba8
Nina <ninacarlotta@gmail.com>
2019-08-16 13:09:41 +0200

    added slideintoview

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-console.log ("hi")
+document.addEventListener("DOMContentLoaded", function (event) {
+    // menu mobile
+    var mobileMenuButton = document.querySelector('.mobile-menu-button')
+    var navigation = document.querySelector('.main-nav')
+    mobileMenuButton.addEventListener('click', function () {
+        mobileMenuButton.classList.toggle('active')
+        navigation.classList.toggle('visible')
+    })
 
-var mobileMenuButton = document.querySelector('.mobile-menu-button')
-  
-// get my nav with querySelector
-var navigation = document.querySelector('.main-nav')
+    // scroll effects
+    navigation.addEventListener('click', function (e) {
+        if (e.target.classList[0].includes('main-nav__item')) {
+            var classes = e.target.classList
+            var item = classes[1].split('-')[0]
+            var section = document.querySelector('#' + item)
+            section.scrollIntoView({behavior: "smooth"})
+        }
+    })
 
+    // scroll effect arrow
+    var arrow = document.querySelector('.downarrow')
 
-// add click listener
-mobileMenuButton.addEventListener('click', function () {
-  
-  // play with css classes
-  
-  mobileMenuButton.classList.toggle('active')
-  navigation.classList.toggle('visible')
-  
-})
+    arrow.addEventListener('click', function (e) {
+        var projects = document.querySelector('#projects')
+        projects.scrollIntoView({behavior: "smooth"})
+    })
+
+    var nextone = document.querySelector('.nextone')
+
+    nextone.addEventListener('click', function (e) {
+        var project2 = document.querySelector('#project2')
+        project2.scrollIntoView({behavior: "smooth"})
+    })
+
+    var nexttwo = document.querySelector('.nexttwo')
+
+    nexttwo.addEventListener('click', function (e) {
+        var project3 = document.querySelector('#project3')
+        project3.scrollIntoView({behavior: "smooth"})
+    })
+
 
 })
